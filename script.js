0e7bcbe96bab87d85316a8a914c2d10005ffc22d
HouliHeeler <luke_houlihan@hotmail.com>
2022-06-24 22:01:25 -0700

    daily kata challenge

--- script.js
+++ script.js
@@ -2201,3 +2201,11 @@ function horMirror(strng) {
 function oper(fct, s) {
   return fct(s)
 }
+
+//Who wins the alphabet war?
+
+function alphabetWar(fight){
+  const values = {'w':4, 'p':3, 'b':2, 's':1,'m':-4, 'q':-3, 'd':-2, 'z':-1}
+  const ans = fight.split("").map(el => values[el] ? values[el]: 0).reduce((a,b) => a + b)
+  return ans == 0 ? "Let's fight again!" : ans < 0 ? "Left side wins!" : "Right side wins!"
+}
